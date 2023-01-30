320ec4e3a011e774186f8c146d188541a8c74df2
HubertKor <98031641+HubertKor@users.noreply.github.com>
2023-01-30 08:50:41 +0100

    HubertKor

---
+++ zadanie2/print.java
@@ -0,0 +1,17 @@
+package zadanie2;
+
+public class print {
+    public static <T> void print(T[] array){
+        for(int i = 0;i< array.length;i++){
+            if(i >= array.length-1){
+                System.out.println(array[i]+",");
+            }else {
+                System.out.println(array[i]);
+            }
+        }
+    }
+    public static void main(String[] args){
+        String[] array = {"abc","def","ghj"};
+        print(array);
+    }
+}
