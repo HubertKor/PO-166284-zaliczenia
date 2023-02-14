6023cc91b7166e5c27a3f91c4ee29e85896a79b6
HubertKor <98031641+HubertKor@users.noreply.github.com>
2023-02-14 11:25:31 +0100

    Create Doktorant.java

---
+++ E2-166284/Doktorant.java
@@ -0,0 +1,31 @@
+package zadanie1termin3a;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Doktorant extends Student implements Named , Clonable{
+    private final int id;
+    private Date dateOfStart = null;
+    private ArrayList<Integer> points = null;
+
+    public Doktorant(String name, int id, ArrayList<Integer> points) {
+        super(name);
+        this.id = id;
+        this.dateOfStart = new Date();
+        this.points = points;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public ArrayList<Integer> getPoints() {
+        return points;
+    }
+
+    public String toString() {
+        return "ID = " + id
+                + ", dateOfStart = " + dateOfStart
+                + ", points = " + points;
+    }
+
+}
