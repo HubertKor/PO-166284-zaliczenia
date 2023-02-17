0af848827ee94105281be40a8ba6a8787f82be11
Oskar Skowroński <97964979+OskarSko@users.noreply.github.com>
2023-02-17 11:15:34 +0100

    Update Main.java

--- E3-166284/src/zadanie1/Main.java
+++ E3-166284/src/zadanie1/Main.java
@@ -8,8 +8,11 @@ import java.util.Vector;
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
         Vector<Java> zad1 = new Vector<>();
-        Java zad2 = new Java(true,new Vector<>(1,2));
+        Java zad2 = new Java(true,new Vector<String>(1,2));
         Java zad3 = new Java(false,new Vector<>(2,3));
         Collections.sort(zad1);
+        for(Java e : zad1){
+            System.out.println(e.getIsObjectOriented() + " " + e.getIsObjectOriented());
+        }
     }
 }
